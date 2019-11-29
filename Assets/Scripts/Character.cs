de2090e6d0e0902085fe0c822be32a5ccf702f4f
TheLazyMan <l.sergey.borzov.l@gmail.com>
2019-11-29 17:55:43 +0300

    Added character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 move_direction = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 movementOffset = new Vector3(0.0f, 0.0f, 0.0f);
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float gravityScale = 5.0f;
+    [SerializeField] private float jumpForce = 2.0f;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        move_direction = new Vector3(Input.GetAxis("Horizontal")*speed, move_direction.y, move_direction.z);
+
+       
+        if (characterController.isGrounded) {
+            if(Input.GetButtonDown("Jump")) {
+                move_direction.y = jumpForce;
+            }
+        }
+
+        move_direction.y += (Physics.gravity.y * gravityScale * Time.deltaTime);
+
+        if (transform.position.z != 0)
+        {
+            movementOffset.z = (0 - transform.position.z) * 0.1f;
+        }
+
+        characterController.Move(movementOffset);
+        characterController.Move(move_direction*Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
