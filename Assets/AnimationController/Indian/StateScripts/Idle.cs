253a9ede19ba61234e5b6ccde47a531ec9de00f4
Dasha Vasilenko <dariavasilenko0702@yandex.ru>
2019-12-02 13:24:45 +0100

    animation2.0. fix state layers

--- Assets/AnimationController/Indian/StateScripts/Idle.cs
+++ Assets/AnimationController/Indian/StateScripts/Idle.cs
@@ -40,27 +40,3 @@ public class Idle : StateData
         }
     }
 }
-
-
-/*
-
-namespace roundbeargames_tutorial
-{
-    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/Idle")]
-    public class Idle : StateData
-    {
-        public override void UpdateAbility(CharacterState characterState, Animator animator)
-        {
-            if (VirtualInputManager.Instance.MoveRight)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), true);
-            }
-
-            if (VirtualInputManager.Instance.MoveLeft)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), true);
-            }
-        }
-    }
-}
- */
--- Assets/AnimationController/Indian/StateScripts/MoveForward.cs
+++ Assets/AnimationController/Indian/StateScripts/MoveForward.cs
@@ -10,7 +10,7 @@ public class MoveForward : StateData
 
     public override void UpdateAbility(CharacterState characterState, Animator animator)
     {
-        CharacterControl c = characterState.GetCharacterControl(animator);
+        CharacterControl control = characterState.GetCharacterControl(animator);
 
         if (VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
         {
@@ -25,51 +25,16 @@ public class MoveForward : StateData
 
         if (VirtualInputManager.Instance.MoveRight)
         {
-            c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-            c.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            control.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            control.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
             //animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), true);
         }
 
         if (VirtualInputManager.Instance.MoveLeft)
         {
-            c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-            c.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            control.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            control.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
             //animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), true);
         }
     }
 }
-/*
-    public class MoveForward : StateData
-    {
-        public float Speed;
-        public override void UpdateAbility(CharacterState characterState, Animator animator)
-        {
-            CharacterControl c = characterState.GetCharacterControl(animator);
-
-            if (VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false);
-                return;
-            }
-
-            if (!VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false);
-                return;
-            }
-
-            if (VirtualInputManager.Instance.MoveRight)
-            {
-                c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-                c.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            }
-
-            if (VirtualInputManager.Instance.MoveLeft)
-            {
-                c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-                c.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-            }
-        }
-    }
-}
-*/
