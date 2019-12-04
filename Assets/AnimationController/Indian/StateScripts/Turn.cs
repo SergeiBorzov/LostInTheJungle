1b4e0e7ce7b3c5e69eb6fbdb83a71c18dd2cc2cd
Dasha Vasilenko <dariavasilenko0702@yandex.ru>
2019-12-04 21:31:17 +0100

    animation2.0. add walkingJump

--- Assets/AnimationController/Indian/StateScripts/Turn.cs
+++ Assets/AnimationController/Indian/StateScripts/Turn.cs
@@ -7,19 +7,7 @@ public class Turn : StateData
 {
     public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
-        /*
-        CharacterControl control = characterState.GetCharacterControl(animator);
 
-        if (control.IsFacingForward())
-        {
-            control.FaceForward(false);
-        }
-        else
-        {
-            control.FaceForward(true);
-        }
-        */
-       //animator.SetBool(CharacterControl.TransitionParameter.Turn.ToString(), false);
     }
 
     public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
