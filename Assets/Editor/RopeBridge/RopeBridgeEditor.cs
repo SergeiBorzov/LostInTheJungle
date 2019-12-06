2f5fe61a76e7276028df96b402287a30fea0fe47
TheLazyMan <l.sergey.borzov.l@gmail.com>
2019-12-06 23:02:04 +0300

    ropeBridge started

---
+++ Assets/Editor/RopeBridge/RopeBridgeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+
+[CustomEditor(typeof(RopeBridge))]
+public class RopeBridgeEditor : Editor
+{
+    // Start is called before the first frame update
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RopeBridge ropeBridge = (RopeBridge)target;
+
+        if (GUILayout.Button("Build Bridge"))
+        {
+            ropeBridge.BuildBridge();
+        }
+
+        if (GUILayout.Button("Destroy Bridge"))
+        {
+            ropeBridge.DestroyBridge();
+        }
+    }
+}
