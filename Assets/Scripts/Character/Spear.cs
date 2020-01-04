5a5d5d68b45c37f7f0e64eee2dbb44f9b32affeb
TheLazyMan <l.sergey.borzov.l@gmail.com>
2020-01-04 14:17:21 +0300

    SpearAnims

---
+++ Assets/Scripts/Character/Spear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    [SerializeField]
+    private Transform rightHandHold;
+    [SerializeField]
+    private Transform spearTransform;
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private void PickSpear()
+    {
+        originalPosition = spearTransform.localPosition;
+        originalRotation = spearTransform.localRotation;
+        spearTransform.SetParent(rightHandHold);
+    }
+
+    private void RemoveSpear()
+    {
+        spearTransform.SetParent(transform);
+        spearTransform.localPosition = originalPosition;
+        spearTransform.localRotation = originalRotation;
+    }
+
+}
